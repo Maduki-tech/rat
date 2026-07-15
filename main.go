1b51c381e8c70a2346da87b8733fa7ae3925fe28
Maduki-tech <d.schlueter1011@gmail.com>
2026-07-15 23:33:38 +0200

    added logging

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello Rat")
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})))
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		slog.Error("Not enough arguments")
+	}
+
+	fileToLookAt := args[0]
+	slog.Debug("Looking at file", "file", fileToLookAt)
 }
